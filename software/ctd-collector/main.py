69abe3e0c9dbcdae50ea0bad8e5b214ee36bd626
Mauro Sbicego <mauro@sbicego.ch>
2024-07-03 17:30:23 +0200

    Initial test with all the sensors except one-wire

--- software/ctd-collector/main.py
+++ software/ctd-collector/main.py
@@ -1,12 +1,33 @@
 import yaml
 from atlas_i2c import commands, sensors, atlas_i2c
 
-sensor_objects = []
+atlas_sensors = []
+bluerobotics_sensors = []
 
 with open("config.yaml") as f:
     cfg = yaml.load(f, Loader=yaml.FullLoader)
 
 if "atlas-sensors" in cfg:
     for sensor_cfg in cfg["atlas-sensors"]:
-        sensor = sensors.Sensor(sensor_cfg["name"], sensor_cfg["address"])
-        sensor_objects.append(sensor)
+        sensor = sensors.Sensor(cfg["atlas-sensors"][sensor_cfg]["name"], cfg["atlas-sensors"][sensor_cfg]["address"])
+        atlas_sensors.append(sensor)
+
+if "bluerobotics-sensors" in cfg:
+    for sensor_cfg in cfg["bluerobotics-sensors"]:
+        #sensor = sensors.Sensor(cfg["bluerobotics-sensors"][sensor_cfg]["name"], cfg["bluerobotics-sensors"][sensor_cfg]["address"])
+        import ms5837
+        # https://github.com/bluerobotics/ms5837-python
+        sensor = ms5837.MS5837() # Use defaults (MS5837-30BA device on I2C bus 1)
+        bluerobotics_sensors.append(sensor)
+
+
+for sensor in atlas_sensors:
+    sensor.connect()
+    response = sensor.query(commands.READ)
+    print(f"Data for {sensor.name}: {response.data}")
+
+for sensor in bluerobotics_sensors:
+    sensor.init()
+    sensor.read(ms5837.OSR_256)
+    print(sensor.pressure())
+    print(sensor.temperature())
