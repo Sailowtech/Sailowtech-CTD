79fc989034c6b9974ddca408f77a051da98fa4ea
Arthur2479 <arthurjacobs1004@gmail.com>
2024-07-24 13:14:35 +0200

    Use default sensors + add some properties (getters and setters)

--- software/sailowtech_ctd/types_/ctd.py
+++ software/sailowtech_ctd/types_/ctd.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum, auto
+from typing import Union
 
 import yaml
 
@@ -32,7 +33,7 @@ class Sensor:
 
 class CTD:
     # I'm sorry for this. Hardcoding all the sensors.
-    SENSORS: list[Sensor] = [
+    DEFAULT_SENSORS: list[Sensor] = [
         Sensor(SensorType.DISSOLVED_OXY, "Dissolved Oxygen", 0x61, SensorBrand.Atlas),
         Sensor(SensorType.CONDUCTIVITY, "Conductivity Probe", 0x64, SensorBrand.Atlas),
         Sensor(SensorType.DISSOLVED_OXY_TEMP, "Temperature from Dissolved Oxygen Sensor", 0x66, SensorBrand.Atlas),
@@ -44,10 +45,27 @@ class CTD:
     def __init__(self, config_path):
         self.name: str = ''
         # self.sensors_config: dict[str, dict[str, dict[str, str]]] = {}
-        self.sensors: list[GenericSensor] = []
+        self._sensors: list[
+            Union[GenericSensor, Sensor]] = []  # For now, accepts generic and Sensor dataclass. Choose one later
         self.interval: float = 0.
 
         # self.load_config(config_path)
+
+    @property
+    def sensors(self):
+        return self._sensors
+
+    @sensors.setter
+    def sensors(self, val):
+        self._sensors = val
+
+    @property
+    def atlas_sensors(self):
+        return [sensor for sensor in self._sensors if sensor.brand == SensorBrand.Atlas]
+
+    @property
+    def bluerobotics_sensors(self):
+        return [sensor for sensor in self._sensors if sensor.brand == SensorBrand.BlueRobotics]
 
     # TODO : hardcoded for now
     # def load_config(self, config_path):
@@ -58,13 +76,22 @@ class CTD:
     #     self.sensors_config = config["sensors"]
     #     self.interval = config["measurements-interval"]
 
+    # def setup_sensors(self):
+    #     for atlas_sensors in self.sensors_config["atlas-sensors"].keys():
+    #         self.sensors.append(AtlasSensor(**self.sensors_config["atlas-sensors"][atlas_sensors]))
+    #
+    #     for blue_robotics_sensors in self.sensors_config["bluerobotics-sensors"].keys():
+    #         self.sensors.append(
+    #             BlueRoboticsSensor(**self.sensors_config["bluerobotics-sensors"][blue_robotics_sensors]))
+
     def setup_sensors(self):
-        for atlas_sensors in self.sensors_config["atlas-sensors"].keys():
-            self.sensors.append(AtlasSensor(**self.sensors_config["atlas-sensors"][atlas_sensors]))
+        self.sensors = self.DEFAULT_SENSORS
 
-        for blue_robotics_sensors in self.sensors_config["bluerobotics-sensors"].keys():
-            self.sensors.append(
-                BlueRoboticsSensor(**self.sensors_config["bluerobotics-sensors"][blue_robotics_sensors]))
+    # todo : check time interval is greater than sum of time limit for each sensor
 
-    def start(self):
+    def measure(self, sensor: Sensor):
         pass
+
+    def start_measurements(self):
+        for sensor in self.sensors:
+            print(f'Measuring {sensor.name} ...')
