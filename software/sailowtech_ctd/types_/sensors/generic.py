5597cc55f7bb0fe2abb07a12f6dbb1464fbba9a7
Arthur2479 <arthurjacobs1004@gmail.com>
2024-08-01 18:10:23 +0200

    Reorder args + str() improvements

--- software/sailowtech_ctd/types_/sensors/generic.py
+++ software/sailowtech_ctd/types_/sensors/generic.py
@@ -19,10 +19,10 @@ class SensorType(Enum):
 class GenericSensor:
     class Commands(Enum):
         ...
-    
-    def __init__(self, sensor_type: SensorType, name: str, address: int, brand: SensorBrand, min_delay: float = 1):
-        self.sensor_type: SensorType = sensor_type
+
+    def __init__(self, brand: SensorBrand, sensor_type: SensorType, name: str, address: int, min_delay: float = 1):
         self.brand: SensorBrand = brand
+        self.sensor_type: SensorType = sensor_type
 
         self.name: str = name
         self.addr: int = address
@@ -31,12 +31,14 @@ class GenericSensor:
         self.last_read: float = 0.
 
     def read_value(self):
-        pass
+        ...
 
     def __str__(self):
         return f'{self.__class__.__name__}(' \
                f'name="{self.name}", ' \
                f'address={hex(self.addr)}' \
+               f'brand={self.brand}' \
+               f'type={self.sensor_type}' \
                f')'
 
     def __repr__(self):
