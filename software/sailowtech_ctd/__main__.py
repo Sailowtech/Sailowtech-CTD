5e5ed03a9c6950b30b306663c3f4ee44e16896b8
Arthur2479 <arthurjacobs1004@gmail.com>
2024-08-02 20:45:54 +0200

    Cleanup

--- software/sailowtech_ctd/__main__.py
+++ software/sailowtech_ctd/__main__.py
@@ -16,17 +16,8 @@ from pprint import pprint
 from types_.ctd import CTD
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-# from types_.ctd import CTD
-
-# from software.sailowtech_ctd.types_.sensors.bluerobotics import BlueRoboticsSensor
-
-
-# from atlas_i2c import commands, sensors, atlas_i2c
 
 if __name__ == '__main__':
-    atlas_sensors = []
-    bluerobotics_sensors = []
-
     ctd = CTD(config_path="config.yaml")
     ctd.setup_sensors()
 
@@ -37,28 +28,3 @@ if __name__ == '__main__':
         ctd.measure_all()
         time.sleep(1)
         i += 1
-
-    # if "atlas-sensors" in cfg:
-    #     for sensor_cfg in cfg["atlas-sensors"]:
-    #         sensor = sensors.Sensor(cfg["atlas-sensors"][sensor_cfg]["name"],
-    #                                 cfg["atlas-sensors"][sensor_cfg]["address"])
-    #         atlas_sensors.append(sensor)
-    #
-    # if "bluerobotics-sensors" in cfg:
-    #     for sensor_cfg in cfg["bluerobotics-sensors"]:
-    #         # sensor = sensors.Sensor(cfg["bluerobotics-sensors"][sensor_cfg]["name"], cfg["bluerobotics-sensors"][sensor_cfg]["address"])
-    #         import ms5837
-    #
-    #         # https://github.com/bluerobotics/ms5837-python
-    #         sensor = ms5837.MS5837()  # Use defaults (MS5837-30BA device on I2C bus 1)
-    #         bluerobotics_sensors.append(sensor)
-
-    # while True:
-    #     for sensor in atlas_sensors:
-    #         sensor.connect()
-    #         response = sensor.query(commands.READ)
-    #         print(f"Data for {sensor.name}: {response.data}")
-    #
-    #     for sensor in bluerobotics_sensors:
-    #         sensor.init()
-    #         sensor.read(ms5837.OSR_256)
