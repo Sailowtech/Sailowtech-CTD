4d3aa055336e5045f3529a1d3bb14e89323e9314
Mauro Sbicego <mauro@sbicego.ch>
2025-01-04 19:33:17 +0100

    Added ensurepath for pipx to work

--- os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
+++ os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 pipx install poetry
+sudo pipx ensurepath -- global
 git clone https://github.com/Sailowtech/Sailowtech-CTD.git ${ROOTFS_DIR}/home/ctd/Sailowtech-CTD
 cd ${ROOTFS_DIR}/home/ctd/Sailowtech-CTD
 poetry install
