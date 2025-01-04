4fe03d0b9e6c15f32a71eae8b35a38059799e381
Mauro Sbicego <34381396+maurosbicego@users.noreply.github.com>
2025-01-04 22:27:00 +0100

    Update 00-run-chroot.sh

--- os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
+++ os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
-sudo pipx install poetry --global || true
+sudo pipx install poetry || true
 sudo pipx ensurepath
+pipx install poetry || true
+pipx ensurepath || true
 git clone https://github.com/Sailowtech/Sailowtech-CTD.git ${ROOTFS_DIR}/home/ctd/Sailowtech-CTD
 cd ${ROOTFS_DIR}/home/ctd/Sailowtech-CTD
-poetry install || true
+/root/.local/bin/poetry install || true
