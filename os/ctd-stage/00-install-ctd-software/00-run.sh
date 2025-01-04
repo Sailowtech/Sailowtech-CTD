8145889f3adbb3b1bb181ba9ffad87d107692761
Mauro Sbicego <mauro@sbicego.ch>
2025-01-04 18:59:29 +0100

    Fixed git clone command, changed job name, and moved installation script to chroot script

--- os/ctd-stage/00-install-ctd-software/00-run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-pipx install poetry
-git clone https://github.com/Sailowtech/Sailowtech-CTD.git -o ${ROOTFS_DIR}/home/ctd/Sailowtech-CTD
-cd ${ROOTFS_DIR}/home/ctd/Sailowtech-CTD
-poetry install
