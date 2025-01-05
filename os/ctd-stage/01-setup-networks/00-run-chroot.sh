54b309210fe3b7a0b856dd19cc203bb7031e2145
Mauro Sbicego <34381396+maurosbicego@users.noreply.github.com>
2025-01-05 22:04:25 +0100

    Create iwd directory

--- os/ctd-stage/01-setup-networks/00-run-chroot.sh
+++ os/ctd-stage/01-setup-networks/00-run-chroot.sh
@@ -2,6 +2,7 @@
 
 sudo systemctl disable wpa_supplicant
 sudo systemctl enable iwd.service
-printf "[Security]\nPassphrase=CTDCTDCTD\n\n[Settings]\nAutoConnect=true\n" | sudo tee /var/lib/iwd/CTD.psk
+sudo mkdir /var/lib/iwd || true
+printf "[Security]\nPassphrase=CTDCTDCTD\n\n[Settings]\nAutoConnect=true\n" | sudo tee /var/lib/iwd/CTD.psk || true
 printf "[device]\nwifi.backend=iwd\n" | sudo tee /etc/NetworkManager/conf.d/iwd.conf || true
 ls /etc/NetworkManager/conf.d/ || true
