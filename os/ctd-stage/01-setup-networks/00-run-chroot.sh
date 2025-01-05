9153ccc8c2ab8b7855755acaddb81106854e5abb
Mauro Sbicego <mauro@sbicego.ch>
2025-01-05 21:30:52 +0100

    Add additional config for iwd

--- os/ctd-stage/01-setup-networks/00-run-chroot.sh
+++ os/ctd-stage/01-setup-networks/00-run-chroot.sh
@@ -3,3 +3,5 @@
 sudo systemctl disable wpa_supplicant
 sudo systemctl enable iwd.service
 printf "[Security]\nPassphrase=CTDCTDCTD\n\n[Settings]\nAutoConnect=true\n" | sudo tee /var/lib/iwd/CTD.psk
+printf "[device]\nwifi.backend=iwd\n" | sudo tee /etc/NetworkManager/conf.d/iwd.conf || true
+ls /etc/NetworkManager/conf.d/ || true
