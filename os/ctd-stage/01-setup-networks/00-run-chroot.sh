7c23be45ddba110170627bebdf684073c10effbd
Mauro Sbicego <mauro@sbicego.ch>
2025-01-05 13:25:16 +0100

    Fixed pipe symbol in network setup

--- os/ctd-stage/01-setup-networks/00-run-chroot.sh
+++ os/ctd-stage/01-setup-networks/00-run-chroot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-sudo wpa_passphrase "CTD" "CTDCTDCTD" || sudo tee /etc/wpa_supplicant/wpa_supplicant.conf
+sudo wpa_passphrase "CTD" "CTDCTDCTD" | sudo tee /etc/wpa_supplicant/wpa_supplicant.conf
