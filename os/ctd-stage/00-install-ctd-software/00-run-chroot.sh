0e81220901c33093dfecb54d4453abdefaa3c9b7
Mauro Sbicego <mauro@sbicego.ch>
2025-01-05 17:08:25 +0100

    Ensure ctd user stays active and change installation of poetry

--- os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
+++ os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-sudo pipx install poetry
-sudo pipx ensurepath
+pipx install poetry
+pipx ensurepath
 git clone https://github.com/Sailowtech/Sailowtech-CTD.git /home/ctd/Sailowtech-CTD
 cd /home/ctd/Sailowtech-CTD
-/root/.local/bin/poetry install
+/root/.local/bin/poetry install || true
