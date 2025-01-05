781d1506b242481996c01d162ce46a43da310119
Mauro Sbicego <mauro@sbicego.ch>
2025-01-05 13:23:36 +0100

    Fix install path and skip creation of stage2 image file

--- os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
+++ os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
 
-sudo pipx install poetry || true
+sudo pipx install poetry
 sudo pipx ensurepath
-pipx install poetry || true
-pipx ensurepath || true
-git clone https://github.com/Sailowtech/Sailowtech-CTD.git ${ROOTFS_DIR}/home/ctd/Sailowtech-CTD
-cd ${ROOTFS_DIR}/home/ctd/Sailowtech-CTD
-/root/.local/bin/poetry install || true
+git clone https://github.com/Sailowtech/Sailowtech-CTD.git /home/ctd/Sailowtech-CTD
+cd /home/ctd/Sailowtech-CTD
+/root/.local/bin/poetry install
