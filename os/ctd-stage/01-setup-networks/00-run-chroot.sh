6a1f715548909888cf3010c904cf09bf2e0e474e
Mauro Sbicego <mauro@sbicego.ch>
2025-01-06 11:17:36 +0100

    Change action so that it runs on tagged versions

--- os/ctd-stage/01-setup-networks/00-run-chroot.sh
+++ os/ctd-stage/01-setup-networks/00-run-chroot.sh
@@ -2,7 +2,6 @@
 
 sudo systemctl disable wpa_supplicant
 sudo systemctl enable iwd.service
-sudo mkdir /var/lib/iwd || true
-printf "[Security]\nPassphrase=CTDCTDCTD\n\n[Settings]\nAutoConnect=true\n" | sudo tee /var/lib/iwd/CTD.psk || true
-printf "[device]\nwifi.backend=iwd\n" | sudo tee /etc/NetworkManager/conf.d/iwd.conf || true
-ls /etc/NetworkManager/conf.d/ || true
+sudo mkdir /var/lib/iwd
+printf "[Security]\nPassphrase=CTDCTDCTD\n\n[Settings]\nAutoConnect=true\n" | sudo tee /var/lib/iwd/CTD.psk
+printf "[device]\nwifi.backend=iwd\n" | sudo tee /etc/NetworkManager/conf.d/iwd.conf
