36e7cfb36634a1024281e8a10473a3e1b3c4e8a0
Mauro Sbicego <mauro@sbicego.ch>
2025-02-05 19:06:24 +0100

    Added debug statements

--- os/ctd-stage/01-setup-networks/00-run-chroot.sh
+++ os/ctd-stage/01-setup-networks/00-run-chroot.sh
@@ -8,10 +8,10 @@ printf "[device]\nwifi.backend=iwd\n" | sudo tee /etc/NetworkManager/conf.d/iwd.
 sudo mkdir -p /var/lib/iwd/ap/
 printf "[Security]\nPassphrase=CTDCTDCTD\n\n[IPv4]\nAddress=192.168.42.1\nGateway=192.168.42.1\nNetmask=255.255.255.0\nDNSList=9.9.9.9" | sudo tee /var/lib/iwd/ap/CTD.ap
 
-sudo iw dev wlan0 interface add ap0 type __ap
-sudo ip link set dev ap0 address b8:27:eb:00:00:00
-sudo systemctl restart iwd
-sudo iwctl device ap0 set-property Mode ap
-sudo iwctl ap ap0 start-profile CTD
+sudo iw dev wlan0 interface add ap0 type __ap || true
+sudo ip link set dev ap0 address b8:27:eb:00:00:00 || true
+sudo systemctl restart iwd || true
+sudo iwctl device ap0 set-property Mode ap || true
+sudo iwctl ap ap0 start-profile CTD || true
 
 # printf "[Security]\nPassphrase=CTDCTDCTD\n\n[Settings]\nAutoConnect=true\n" | sudo tee /var/lib/iwd/CTD.psk
