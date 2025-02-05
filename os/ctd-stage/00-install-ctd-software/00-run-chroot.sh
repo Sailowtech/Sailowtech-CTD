1e3b557df355c862c2907a27afb7454623063b8b
Mauro Sbicego <mauro@sbicego.ch>
2025-02-05 18:03:46 +0100

    chown git directory to avoid errors with permissions

--- os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
+++ os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
@@ -4,5 +4,6 @@
 pipx install poetry
 pipx ensurepath
 git clone https://github.com/Sailowtech/Sailowtech-CTD.git /home/ctd/Sailowtech-CTD
+sudo chown -R ctd:ctd /home/ctd/Sailowtech-CTD
 cd /home/ctd/Sailowtech-CTD
 /root/.local/bin/poetry install || true
