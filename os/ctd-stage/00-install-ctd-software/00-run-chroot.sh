cc706a3faffb2e6f4029c006d24a9adb618bf1ce
Mauro Sbicego <mauro@sbicego.ch>
2025-02-06 18:21:58 +0100

    Fix poetry install and path in image build

--- os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
+++ os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-
 pipx install poetry
 pipx ensurepath
 git clone https://github.com/Sailowtech/Sailowtech-CTD.git /home/ctd/Sailowtech-CTD
 sudo chown -R ctd:ctd /home/ctd/Sailowtech-CTD
 cd /home/ctd/Sailowtech-CTD
-/root/.local/bin/poetry install || true
+printf "# Sailowtech-CTD: install folder for poetry\nexport PATH=\"$PATH:/opt/pipx_bin\"" | sudo tee -a /home/ctd/.bashrc
+/opt/pipx_bin/poetry install || true
