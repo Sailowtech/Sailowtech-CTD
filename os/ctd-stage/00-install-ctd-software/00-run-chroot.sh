e470161c528e424d45a4de7a1f94f8826f5a8bf5
Mauro Sbicego <mauro@sbicego.ch>
2025-02-11 21:43:26 +0100

    Correctly setup poetry as ctd user

--- os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
+++ os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
@@ -7,5 +7,5 @@ git clone https://github.com/Sailowtech/Sailowtech-CTD.git /home/ctd/Sailowtech-
 sudo chown -R ctd:ctd /home/ctd/Sailowtech-CTD
 cd /home/ctd/Sailowtech-CTD
 printf "# Sailowtech-CTD: install folder for poetry\nexport PATH=\"$PATH:/opt/pipx_bin\"" | sudo tee -a /home/ctd/.bashrc
-/opt/pipx_bin/poetry env use /home/ctd/.pyenv/versions/3.12.8/bin/python3
-/opt/pipx_bin/poetry install || true
+sudo -u ctd /opt/pipx_bin/poetry env use /home/ctd/.pyenv/versions/3.12.8/bin/python3
+sudo -u ctd /opt/pipx_bin/poetry install
