3d6cc72046e9777761b0361a170ff479e36ce165
Mauro Sbicego <mauro@sbicego.ch>
2025-02-11 19:45:33 +0100

    Install pyenv and python version as ctd user

--- os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
+++ os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
 
-curl -fsSL https://pyenv.run | bash
-/root/.pyenv/bin/pyenv install 3.12.8 # required python version for PonyORM to work
+curl -fsSL https://pyenv.run | sudo -u ctd bash
+sudo -u ctd /home/ctd/.pyenv/bin/pyenv install 3.12.8 # required python version for PonyORM to work
 pipx install poetry
-pipx ensurepath
 git clone https://github.com/Sailowtech/Sailowtech-CTD.git /home/ctd/Sailowtech-CTD
 sudo chown -R ctd:ctd /home/ctd/Sailowtech-CTD
 cd /home/ctd/Sailowtech-CTD
 printf "# Sailowtech-CTD: install folder for poetry\nexport PATH=\"$PATH:/opt/pipx_bin\"" | sudo tee -a /home/ctd/.bashrc
-/opt/pipx_bin/poetry env use /root/.pyenv/versions/3.12.8/bin/python3
+/opt/pipx_bin/poetry env use /home/ctd/.pyenv/versions/3.12.8/bin/python3
 /opt/pipx_bin/poetry install || true
