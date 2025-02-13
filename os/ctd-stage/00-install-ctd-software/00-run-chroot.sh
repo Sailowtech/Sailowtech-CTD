7d552a71a7c7f02b7fddd7059d6f06fee887a98c
Mauro Sbicego <mauro@sbicego.ch>
2025-02-13 19:28:38 +0100

    Add frontend build to image build process

--- os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
+++ os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
@@ -12,3 +12,7 @@ sudo -u ctd /opt/pipx_bin/poetry install
 
 (printf "[Unit]\nDescription=Sailowtech-CTD Web Service\n\n[Service]\nAmbientCapabilities=CAP_NET_BIND_SERVICE\nUser=ctd\nGroup=ctd\nExecStart=" && poetry env list --full-path | grep -Po ".* " | sed 's/.$//' | sed 's/$/\/bin\/web/' && printf "\n[Install]\nWantedBy=multi-user.target\n") | sudo tee /etc/systemd/system/sailowtech-ctd-web.service
 sudo systemctl enable sailowtech-ctd-web
+
+cd /home/ctd/Sailowtech-CTD/software/sailowtech-ctd-frontend/
+sudo -u ctd npm install
+sudo -u ctd npm run build
