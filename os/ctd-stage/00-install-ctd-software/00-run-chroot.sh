9c32fc93fac2d89958ccc45f8b00713965a37fe0
Mauro Sbicego <mauro@sbicego.ch>
2025-02-13 15:46:22 +0100

    Create autostart file

--- os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
+++ os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
@@ -9,3 +9,6 @@ cd /home/ctd/Sailowtech-CTD
 printf "# Sailowtech-CTD: install folder for poetry\nexport PATH=\"$PATH:/opt/pipx_bin\"" | sudo tee -a /home/ctd/.bashrc
 sudo -u ctd /opt/pipx_bin/poetry env use /home/ctd/.pyenv/versions/3.12.8/bin/python3
 sudo -u ctd /opt/pipx_bin/poetry install
+
+(printf "[Unit]\nDescirption=Sailowtech-CTD Web Service\n\n[Service]\nAmbientCapabilities=CAP_NET_BIND_SERVICE\nUser=ctd\nGroup=ctd\nExecStart=" && poetry env list --full-path | grep -Po ".* " | sed 's/.$//' | sed 's/$/\/bin\/web/' && printf "\n[Install]\nWantedBy=multi-user.target\n") | sudo tee /etc/systemd/system/sailowtech-ctd-web.service
+sudo systemctl enable sailowtech-ctd-web
