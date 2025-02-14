4df5d4a85c598aab1ecdf9dab89558fa66050cfa
Mauro Sbicego <mauro@sbicego.ch>
2025-02-14 09:28:31 +0100

    Fix systemd service file creation

--- os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
+++ os/ctd-stage/00-install-ctd-software/00-run-chroot.sh
@@ -10,7 +10,7 @@ printf "# Sailowtech-CTD: install folder for poetry\nexport PATH=\"$PATH:/opt/pi
 sudo -u ctd /opt/pipx_bin/poetry env use /home/ctd/.pyenv/versions/3.12.8/bin/python3
 sudo -u ctd /opt/pipx_bin/poetry install
 
-(printf "[Unit]\nDescription=Sailowtech-CTD Web Service\n\n[Service]\nAmbientCapabilities=CAP_NET_BIND_SERVICE\nUser=ctd\nGroup=ctd\nExecStart=" && poetry env list --full-path | grep -Po ".* " | sed 's/.$//' | sed 's/$/\/bin\/web/' && printf "\n[Install]\nWantedBy=multi-user.target\n") | sudo tee /etc/systemd/system/sailowtech-ctd-web.service
+(printf "[Unit]\nDescription=Sailowtech-CTD Web Service\n\n[Service]\nAmbientCapabilities=CAP_NET_BIND_SERVICE\nUser=ctd\nGroup=ctd\nExecStart=" && sudo -u ctd /opt/pipx_bin/poetry env list --full-path | grep -Po ".* " | sed 's/.$//' | sed 's/$/\/bin\/web/' && printf "\n[Install]\nWantedBy=multi-user.target\n") | sudo tee /etc/systemd/system/sailowtech-ctd-web.service
 sudo systemctl enable sailowtech-ctd-web
 
 cd /home/ctd/Sailowtech-CTD/software/sailowtech-ctd-frontend/
